b9aae1a761ef1aa894690302ed78c25e4a999abc
James Drake <info@jamesmdrakemedia.com>
2025-04-25 15:25:13 -0400

    feat: added timeout to tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,6 +6,7 @@ const server = require("../server");
 chai.use(chaiHttp);
 
 suite("Functional Tests", function () {
+  this.timeout(5000);
   test("Create an issue with every field: POST request to /api/issues/{project}", function (done) {});
   test("Create an issue with only required fields: POST request to /api/issues/{project}", function (done) {});
   test("Create an issue with missing required fields: POST request to /api/issues/{project}", function (done) {});
