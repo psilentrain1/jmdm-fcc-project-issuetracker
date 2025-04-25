a0a0b4311197abc9df7f44f8e9c869a451ef0a2e
James Drake <info@jamesmdrakemedia.com>
2025-04-25 15:42:26 -0400

    feat: mongoose schema and model

--- routes/api.js
+++ routes/api.js
@@ -6,6 +6,27 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// SCHEMAS
+const issueSchema = new mongoose.Schema({
+  issue_title: { type: String, required: true },
+  issue_text: { type: String, required: true },
+  created_on: { type: Date, default: Date.now },
+  updated_on: { type: Date, default: Date.now },
+  created_by: { type: String, required: true },
+  assigned_to: String,
+  status_text: String,
+  open: { type: Boolean, default: true },
+  project: { type: String, required: true },
+});
+
+// MODELS
+const Issue = mongoose.model("Issue", issueSchema);
+
 module.exports = function (app) {
   app
     .route("/api/issues/:project")
